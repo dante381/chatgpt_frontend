a9b8c3ccd6c644d7d82abe9c5750b97254662656
naveenvarma600 <naveenvarma600@gmail.com>
2023-02-13 09:41:37 +0530

    added css module stylesheets

--- src/components/login.js
+++ src/components/login.js
@@ -1,29 +1,43 @@
 import { Component } from 'react';
 import './login.css'
+import styles from './login.module.css'
 
 class Login extends Component{
     constructor(){
         super();
         this.state={
-            message:"Login here"
+            username:"",
+            password:""
         }
     }
-    changemessage(){
+    handleUsernameChange = (event)=>{
         this.setState({
-            message:" Logged In "
+            username : event.target.value
         })
+    }
+    handlePasswordChange = (event)=>{
+        this.setState({
+            password : event.target.value
+        })
+    }
+    handleSubmit = () => {
+        alert(`${this.state.username} ${this.state.password}`)
     }
     render(){
         return(
             <div class="logindiv">
-                <h1>{this.state.message}</h1>
-                
-                <input type="text" placeholder="username"></input>
-                <br></br>
-                <input type="password" placeholder="password"></input>
-                <br></br>
-                <button onClick={()=>this.changemessage()}>Login </button>
-                <br></br>
+                <form onSubmit={this.handleSubmit}>
+                    <h1 className={styles.textcolor}> Login </h1>
+                    
+                    <label>Username</label>
+                    <input type="text" value={this.state.username} onChange={this.handleUsernameChange}></input>
+                    <br></br>
+                    <label>Password</label>
+                    <input type="password" value={this.state.password} onChange={this.handlePasswordChange}></input>
+                    <br></br>
+                    <button type="submit">Signup </button>
+                    <br></br>
+                </form>
             </div>
         );
     }
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,30 +1,53 @@
 import { Component } from 'react';
 import './signup.css';
+import styles from './signup.module.css'
 
 class Signup extends Component{
     constructor(){
         super();
         this.state={
-            message:"Sign up Here"
+            message:"Sign up Here",
+            name:"",
+            username:"",
+            password:""
         }
     }
-    changemessage(){
+    handleNameChange = (event)=>{
         this.setState({
-            message:" Created account"
+            name : event.target.value
         })
+    }
+    handleUsernameChange = (event)=>{
+        this.setState({
+            username : event.target.value
+        })
+    }
+    handlePasswordChange = (event)=>{
+        this.setState({
+            password : event.target.value
+        })
+    }
+    handleSubmit = (event)=>{
+        alert(`${this.state.name} ${this.state.username} ${this.state.password}`)
+        event.preventDefault()
     }
     render(){
         return(
             <div class="signupdiv">
-                <h1> {this.state.message} </h1>
-                <input type="text" placeholder="Name"></input>
-                <br></br>
-                <input type="text" placeholder="username"></input>
-                <br></br>
-                <input type="password" placeholder="password"></input>
-                <br></br>
-                <button onClick={()=>this.changemessage()}>Signup </button>
-                <br></br>
+                <form onSubmit={this.handleSubmit}>
+                    <h1 className={styles.textcolor}> Signup Page </h1>
+                    <label>Name</label>
+                    <input type="text" value={this.state.name} onChange={this.handleNameChange}></input>
+                    <br></br>
+                    <label>Username</label>
+                    <input type="text" value={this.state.username} onChange={this.handleUsernameChange}></input>
+                    <br></br>
+                    <label>Password</label>
+                    <input type="password" value={this.state.password} onChange={this.handlePasswordChange}></input>
+                    <br></br>
+                    <button type="submit">Signup </button>
+                    <br></br>
+                </form>
             </div>
         );
     }
